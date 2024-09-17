982e3a00964ecaf0053d710de97b1ef4deb6e1a4
WistonTravieso <wistontravieso@gmail.com>
2024-09-17 11:47:32 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whorandom = Math.floor(Math.random() * who.length);
+    let actionrandom = Math.floor(Math.random() * action.length);
+    let whatrandom = Math.floor(Math.random() * what.length);
+    let whenrandom = Math.floor(Math.random() * when.length);
+
+    document.getElementById("excuse").innerHTML =
+      who[whorandom] +
+      action[actionrandom] +
+      what[whatrandom] +
+      when[whenrandom];
+  }
+
+  generateExcuse();
+
   console.log("Hello Rigo from the console!");
 };
